81580df05f50679a6309bdae6913c908f2f7c418
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-06 13:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstChar(text) {
   // your code here
+	// Trim the input string to remove leading and trailing spaces
+  text = text.trim();
+
+  // Check if the trimmed string is empty
+  if (text === '') {
+    return '';
+  }
+
+  // Iterate through each character of the trimmed string
+  for (let i = 0; i < text.length; i++) {
+    // Check if the current character is not a space
+    if (text[i] !== ' ') {
+      // Return the first non-space character
+      return text[i];
+    }
 }
 
 // Do not change the code below
